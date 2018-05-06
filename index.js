bde69f9bf03db3d0f61c830491bc34349e182294
Eddie Melo <heriberto@melo.nyc>
2018-05-06 18:28:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,9 +8,9 @@ function nestedTarget(){
 
 function increaseRankBy(n){
   let increase = parseInt(n);
-  const list = document.getElementById('app').querySelectorAll('ul.unranked-list li')
+  const list = document.getElementById('app').querySelectorAll('ul.ranked-list li')
 
   for (let i = 0; i < list.length; i++){
-    list[i].innerHTML = (i).toString();
+    list[i].innerHTML = (i + increase).toString();
   }
 }
