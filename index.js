66fb00aa2248b3b4a7c0cc88aebe0427dd745776
Eddie Melo <heriberto@melo.nyc>
2018-05-06 20:07:56 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,22 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector)
+}
+
+function nestedTarget(){
+  return document.querySelector('#nested .target')
+}
+
+function increaseRankBy(n){
+  let increase = parseInt(n);
+  const list = document.querySelectorAll('ul.ranked-list li')
+
+  for (let i = 0; i < list.length; i++){
+    list[i].innerHTML = increase + parseInt(list[i].innerHTML);
+  }
+}
+
+function deepestChild(){
+  const nodelist = document.getElementById('grand-node').querySelectorAll('div');
+  const divyArray = Array.from(nodelist)
+  return divyArray[divyArray.length - 1];
+}
