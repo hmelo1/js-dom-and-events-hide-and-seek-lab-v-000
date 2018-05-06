e35e18dd37498efb61e50b7718fe055dd7d4fc40
Eddie Melo <heriberto@melo.nyc>
2018-05-06 20:05:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function nestedTarget(){
 
 function increaseRankBy(n){
   let increase = parseInt(n);
-  const list = document.getElementById('app').querySelectorAll('ul.ranked-list li')
+  const list = document.querySelectorAll('ul.ranked-list li')
 
   for (let i = 0; i < list.length; i++){
     list[i].innerHTML = increase + parseInt(list[i].innerHTML);
@@ -16,5 +16,5 @@ function increaseRankBy(n){
 }
 
 function deepestChild(){
-  return document.querySelectorAll('#grand-node div').lastChild;
+  return document..getElementById('grand-node"').querySelectorAll('div').lastChild;
 }
