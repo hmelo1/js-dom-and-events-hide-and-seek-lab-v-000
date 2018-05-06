f4ec57320de1cd625b2ca15c59e7d3a3a26eb04b
Eddie Melo <heriberto@melo.nyc>
2018-05-06 18:27:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,6 +11,6 @@ function increaseRankBy(n){
   const list = document.getElementById('app').querySelectorAll('ul.unranked-list li')
 
   for (let i = 0; i < list.length; i++){
-    list[i].innerHTML = (i + n).toString();
+    list[i].innerHTML = (i + increase).toString();
   }
 }
