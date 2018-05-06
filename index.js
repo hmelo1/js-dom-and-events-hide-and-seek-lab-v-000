180365f2f0af7672a3d8052430cd02129c88a60b
Eddie Melo <heriberto@melo.nyc>
2018-05-06 20:06:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,5 +16,5 @@ function increaseRankBy(n){
 }
 
 function deepestChild(){
-  return document.getElementById('grand-node').querySelectorAll('div').lastChild;
+  return document.getElementById('grand-node').querySelectorAll('div');
 }
